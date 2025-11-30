05c36c2552fb83d5840048fa464d6ac1a10419ec
alkeity <alkeity.23@gmail.com>
2025-11-30 15:06:19 +0300

    classwork

---
+++ src/net/alkeity/helloworld/Main.java
@@ -0,0 +1,11 @@
+package net.alkeity.helloworld;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student person = new Student("Alex");
+        person.sayHello();
+    }
+}
